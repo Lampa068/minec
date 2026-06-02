275050bd533dee7fd60c612347f919688a48598e
Lampa068 <93820253+Lampa068@users.noreply.github.com>
2026-06-02 14:11:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,22 @@
 const WebSocket = require('ws');
-const mc = require('minecraft-protocol');
 const mineflayer = require('mineflayer');
 
-console.clear();
-console.log('=== СТАРТ ХМАРНОГО PROXY НА RENDER ===');
-
-const TARGET_HOST = 'mc.sakeva.fun';
-const TARGET_PORT = 25565;
-const PORT = process.env.PORT || 10000;
-
-const wss = new WebSocket.Server({ port: PORT });
-console.log(`Хмарний server чекає на з'єднання через порт ${PORT}...`);
+const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
 
 wss.on('connection', (ws) => {
-  console.log('[Хмара] ПК підключився до хмаринки!');
-
-  const targetClient = mc.createClient({
-    host: TARGET_HOST,
-    port: TARGET_PORT,
-    username: 'Lampa752',
-    auth: 'offline',
-    version: '1.21.11'
-  });
-
-  // Приймаємо бінарні пакети від ПК і шлемо на Сакеву
-  ws.on('message', (msg) => {
-    if (targetClient.state) {
-      targetClient.writeRaw(msg);
+  ws.on('message', (data) => {
+    const msg = JSON.parse(data);
+    if (msg.type === 'takeover') {
+      console.log('📥 Отримав сесію від ПК. Запускаю бота...');
+      
+      const bot = mineflayer.createBot({
+        host: 'mc.sakeva.fun',
+        port: 25565,
+        username: msg.username,
+        version: '1.21.11'
+      });
+      
+      bot.on('spawn', () => console.log('🤖 БОТ В ГРІ (після капчі)!'));
     }
-  });
-
-  // Перехоплюємо сирі пакети від Сакеви і шлемо на ПК
-  targetClient.on('raw', (buffer) => {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.send(buffer);
-    }
-  });
-
-  ws.on('close', () => {
-    console.log('\n[УСПІХ] Ти вийшов з гри на ПК. Передаю керування боту Mineflayer...');
-
-    const bot = mineflayer.createBot({
-      client: targetClient,
-      username: 'Lampa752'
-    });
-
-    bot.on('spawn', () => {
-      console.log('🤖 БОТ УСПІШНО ЗАФІКСОВАНИЙ НА СЕРВЕРІ СУПЕР-ХМАРОЮ!');
-    });
-
-    bot.on('message', (message) => console.log(`[Чат бота]: ${message.toAnsi()}`));
-    bot.on('error', (err) => console.log('❌ Помилка бота:', err.message));
-    bot.on('kicked', (reason) => console.log('❌ Бота кікнуло:', reason));
   });
 });
